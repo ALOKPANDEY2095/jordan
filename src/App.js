ef5184b2508725ae188ae43bdafdcd9d494ff273
ALOKPANDEY05498 <alokpandey2095.com>
2022-04-29 09:05:01 +0000

    commit code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import DailyExpensesCard from "./components/DailyExpensesCard";
+import DashBoard from "./components/DashBoard";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import "./App.css";
+
+const App = () => (
+  <>
+    <DashBoard />
+    <DailyExpensesCard />
+  </>
+);
 
 export default App;
